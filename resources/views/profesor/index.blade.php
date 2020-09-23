ebac86e5def7e45f082f24d49a0e4f0c4fcf5cd4
deivisjl <deivisjl@gmail.com>
2020-09-23 10:41:35 -0600

    Condicion de asignacion vacia

--- resources/views/profesor/index.blade.php
+++ resources/views/profesor/index.blade.php
@@ -16,34 +16,40 @@
                   </div>
               <!-- /.card-header -->
               <div class="card-body">
-                @foreach($registros as $index => $registro)
-                  <div class="card card-default {{ $index > 0 ? 'collapsed-card' : '' }}">
-                      <div class="card-header-custom">
-                          <button type="button" class="btn btn-outlined-default float-left" data-card-widget="collapse">
-                              <strong class="text-danger">{{ $registro['aulas']['carrera'] }}</strong>
-                          </button>
-                      </div>
-                          
-                      <div class="card-body">
-                          @foreach($registro['aulas']['grados'] as $item)
-                          <div class="card">
-                            <div class="footer p-0">
-                                <ul class="nav flex-column">
-                                  <li class="nav-item">
-                                      <a href="{{ route('profesores.show',$item->id) }}" class="nav-link">
-                                          {{ $item->grado }}, Seccion {{ $item->seccion }}
-                                          <div style="vertical-align:middle; margin-right: 10px; margin-bottom:5px; border-radius:50%; background-color:#28a745; color:#fff; width:35px;height:35px;align-items:center;display:inline-flex;float:left">
-                                              <i class="fas fa-book-open fa-1x" style="text-align:center;margin:auto"></i>
-                                          </div>
-                                      </a>
-                                  </li>
-                                </ul>
-                            </div>
+                @if(sizeof($registros) < 1)
+                <div class="callout callout-info">
+                  <h5>No hay registros asociados</h5>
+                </div>
+                @else
+                    @foreach($registros as $index => $registro)
+                      <div class="card card-default {{ $index > 0 ? 'collapsed-card' : '' }}">
+                          <div class="card-header-custom">
+                              <button type="button" class="btn btn-outlined-default float-left" data-card-widget="collapse">
+                                  <strong class="text-danger">{{ $registro['aulas']['carrera'] }}</strong>
+                              </button>
                           </div>
-                          @endforeach
+                              
+                          <div class="card-body">
+                              @foreach($registro['aulas']['grados'] as $item)
+                              <div class="card">
+                                <div class="footer p-0">
+                                    <ul class="nav flex-column">
+                                      <li class="nav-item">
+                                          <a href="{{ route('profesores.show',$item->id) }}" class="nav-link">
+                                              {{ $item->grado }}, Seccion {{ $item->seccion }}
+                                              <div style="vertical-align:middle; margin-right: 10px; margin-bottom:5px; border-radius:50%; background-color:#28a745; color:#fff; width:35px;height:35px;align-items:center;display:inline-flex;float:left">
+                                                  <i class="fas fa-book-open fa-1x" style="text-align:center;margin:auto"></i>
+                                              </div>
+                                          </a>
+                                      </li>
+                                    </ul>
+                                </div>
+                              </div>
+                              @endforeach
+                          </div>
                       </div>
-                  </div>
-                @endforeach
+                  @endforeach
+                @endif
               </div>
               <!-- /.card-body -->
             </div>
